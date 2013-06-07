f49a243f56e0ed7489d91fbb7f93edf8885e5f21
unknown <dzmitry_lahoda@XPBYMINW21471.minsk.epam.com>
2013-06-07 19:00:20 +0300

    clean up

--- src/DceRpc.Idl/ExplicitWithCallbacks_c.c
+++ src/DceRpc.Idl/ExplicitWithCallbacks_c.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Fri Jun 07 16:33:44 2013
+/* at Fri Jun 07 18:58:05 2013
  */
 /* Compiler settings for ExplicitWithCallbacks.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 

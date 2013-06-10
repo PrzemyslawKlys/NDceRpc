f8fc409eac7741726c38502d574b321e4ef42616
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-06-10 19:19:37 +0300

    started to add ALPC

---
+++ src/NAlpc/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAlpc
+{
+    class Constants
+    {
+        /// <summary>
+        /// Maximum number of bytes that can be copied through LPC
+        /// </summary>
+        public const int MAX_LPC_DATA = 0x130;
+    }
+}

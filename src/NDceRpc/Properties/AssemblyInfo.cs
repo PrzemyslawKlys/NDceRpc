22b3e32e061f72c473bedd0921733b01f1d6ed2a
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-06-14 18:13:17 +0300

    vers

--- src/NDceRpc/Properties/AssemblyInfo.cs
+++ src/NDceRpc/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyDescription("Library for interop of managed code with DCE RPC runtime")]
-[assembly: AssemblyVersion("0.14.2.0")]
-[assembly: AssemblyFileVersion("0.14.2.0")]
+[assembly: AssemblyVersion("0.14.2.1")]
+[assembly: AssemblyFileVersion("0.14.2.1")]
 //[assembly:Guid("777CFB6A-6F8D-4D3D-BA80-A0F5EEC0269C")]

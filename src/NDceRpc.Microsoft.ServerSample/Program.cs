27370941a69978bc4e10ba14220f7a4d7440cca2
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2013-08-13 15:58:51 +0300

    made sample of 32 64 bit communication

---
+++ src/NDceRpc.Microsoft.ServerSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NDceRpc.ExplicitBytes;
+using NDceRpc.Microsoft.Interop;
+
+
+namespace NDceRpc.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iid = Guid.Parse("FF9B1856-934A-459B-92AF-18AEBD745BC1");
+            Console.WriteLine("Server id = " + iid);
+            var server = new ExplicitBytesServer(iid);
+            server.AddProtocol(RpcProtseq.ncacn_np, "\\pipe\\testnamedpipe" + iid, byte.MaxValue);
+            server.OnExecute += (client, data) =>
+                {
+                    Console.WriteLine(Encoding.Unicode.GetString(data));
+                    return Encoding.Unicode.GetBytes("Server response");
+                };
+            server.StartListening();
+            Console.WriteLine("Server started");
+            
+
+            Console.ReadKey();
+        }
+    }
+}

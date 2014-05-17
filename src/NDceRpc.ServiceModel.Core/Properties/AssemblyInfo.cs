d18c7ad9f7ce0dc479a303898c4af2f61347d031
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2014-05-17 23:55:16 +0300

    separating contracts out of WCF

---
+++ src/NDceRpc.ServiceModel.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+
+[assembly: AssemblyTitle("NDceRpc.ServiceModel.Core")]
+[assembly: AssemblyDescription("Contains WCF like attribute based contract model for reuse")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NDceRpc.ServiceModel.Core")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]

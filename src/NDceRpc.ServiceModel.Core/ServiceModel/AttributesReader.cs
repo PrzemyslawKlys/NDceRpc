1ab70da58bfa1c790a27fba3eff2b06eb8a95026
Dzmitry Lahoda <Dzmitry.Lahoda@gmail.com>
2014-05-23 13:17:08 +0300

    made wcf services compatible again

--- src/NDceRpc.ServiceModel.Core/ServiceModel/AttributesReader.cs
+++ src/NDceRpc.ServiceModel.Core/ServiceModel/AttributesReader.cs
@@ -1,33 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Text;
+using System.Runtime.CompilerServices;
+
 
 namespace NDceRpc.ServiceModel
 {
     internal class AttributesReader
     {
+
+        private static string wcfNs = "System.ServiceModel.";
+        private static string wcfService = wcfNs + "ServiceContract";
+
         public static ServiceContractAttribute GetServiceContract(Type contractType)
         {
-            return (ServiceContractAttribute)contractType.GetCustomAttributes(typeof(ServiceContractAttribute), false).SingleOrDefault();
+            var attrs = contractType.GetCustomAttributes(false);
+            var attr = attrs.Where(x => x.GetType() == typeof (ServiceContractAttribute)).SingleOrDefault();
+
+            if (attr == null)
+            {
+                attr = attrs.Where(x => x.GetType().FullName.StartsWith(wcfService)).SingleOrDefault();
+                attr = wcfServiceToCustom(attr);
+            }
+            return attr as ServiceContractAttribute;
         }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static object wcfServiceToCustom(object attr)
+         {
+             var wcf = (System.ServiceModel.ServiceContractAttribute) attr;
+             return new ServiceContractAttribute
+             {
+                 CallbackContract = wcf.CallbackContract,
+                 ConfigurationName = wcf.ConfigurationName,
+                 Name = wcf.Name,
+                 Namespace = wcf.Namespace,
+                 ProtectionLevel = wcf.ProtectionLevel,
+                 SessionMode = (SessionMode) (int) wcf.SessionMode
+             };
+         }
+
+       
+      
 
 
         public static OperationContractAttribute GetOperationContract(MethodInfo info)
         {
-            return (OperationContractAttribute)info.GetCustomAttributes(typeof(OperationContractAttribute), false).SingleOrDefault();
+            return GetCustomAttribute<OperationContractAttribute>(info); ;
         }
 
         public static ServiceBehaviorAttribute GetServiceBehavior(object service)
         {
-            return (ServiceBehaviorAttribute)service.GetType().GetCustomAttributes(typeof(ServiceBehaviorAttribute), false).SingleOrDefault();
+            return GetCustomAttribute<ServiceBehaviorAttribute>(service.GetType());
         }
 
 
         public static CallbackBehaviorAttribute GetCallbackBehavior(Type type)
         {
-            return (CallbackBehaviorAttribute)type.GetCustomAttributes(typeof(CallbackBehaviorAttribute), false).SingleOrDefault();
+            return GetCustomAttribute<CallbackBehaviorAttribute>(type);
+        }
+
+        public static T GetCustomAttribute<T>(Type type)
+        {
+            return (T)(type.GetCustomAttributes(typeof(T), false).SingleOrDefault());
         }
 
         public static T GetCustomAttribute<T>(MethodBase methodInfo)
